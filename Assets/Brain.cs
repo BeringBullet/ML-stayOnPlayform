fb42b1e1cbe222075091957042f8d684bcef5959
Mike Walters <mike@beringbullet.com>
2019-06-15 22:49:14 -0500

    Almost working

---
+++ Assets/Brain.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+
+    int DNALength = 2;
+    public float timeAlive;
+    public DNA dna;
+    public GameObject eyes;
+    bool alive = true;
+    bool seeGround = true;
+
+
+    private void OnCollisionEnter(Collision obj)
+    {
+        if (obj.gameObject.tag == "dead")
+        {
+            alive = false;
+        }
+    }
+
+    public void Init()
+    {
+        //initialise DNA
+        //0 forward
+        //1 left
+        //2 right
+        dna = new DNA(DNALength, 3);
+        timeAlive = 0;
+        alive = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!alive) return;
+
+        Debug.DrawLine(eyes.transform.position, eyes.transform.forward * 10, Color.red, 10);
+        seeGround = false;
+        RaycastHit hit;
+        if (Physics.Raycast(eyes.transform.position, eyes.transform.forward * 10, out hit))
+        {
+            if (hit.collider.gameObject.tag == "platform")
+            {
+                seeGround = true;
+            }
+        }
+        timeAlive = PopulationManager.elapsed;
+
+        //read DNA
+        float turn = 0;
+        float move = 0;
+        if (seeGround)
+        {
+            //make v relative to character and alway move forward
+            if (dna.GetGene(0) == 0) move = 1;
+            else if (dna.GetGene(0) == 1) turn = -90;
+            else if (dna.GetGene(0) == 1) turn = 90;
+        }
+        else
+        {
+            if (dna.GetGene(1) == 0) move = 1;
+            else if (dna.GetGene(1) == 1) turn = -90;
+            else if (dna.GetGene(1) == 1) turn = 90;
+        }
+
+        this.transform.Translate(0, 0, move * 0.1f);
+        this.transform.Rotate(0, turn, 0);
+    }
+}
